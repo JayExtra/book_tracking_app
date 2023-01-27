9f34bc1de75bbb477abd47a4313fce827da11658
James Gitonga <40741293+JayExtra@users.noreply.github.com>
2023-01-27 14:47:08 +0300

    - circular progress tracker added - support for animation in circular progress

--- compose-ui/src/main/java/com/dev/james/booktracker/compose_ui/ui/theme/Theme.kt
+++ compose-ui/src/main/java/com/dev/james/booktracker/compose_ui/ui/theme/Theme.kt
@@ -1,11 +1,24 @@
 package com.dev.james.booktracker.compose_ui.ui.theme
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorPalette = darkColors(
@@ -55,3 +68,7 @@ fun BookTrackerTheme(
         content = content
     )
 }
+
+
+
+
--- feature/on-boarding/src/main/java/com/dev/james/booktracker/on_boarding/ui/screens/OnboardingWelcomeScreen.kt
+++ feature/on-boarding/src/main/java/com/dev/james/booktracker/on_boarding/ui/screens/OnboardingWelcomeScreen.kt
@@ -2,15 +2,15 @@
 
 package com.dev.james.booktracker.on_boarding.ui.screens
 
-import android.widget.Toast
-import androidx.annotation.RawRes
 import androidx.compose.animation.*
 import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.VisibilityThreshold
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
@@ -23,14 +23,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.airbnb.lottie.compose.*
 import com.dev.james.booktracker.compose_ui.ui.theme.BookAppTypography
 import com.dev.james.booktracker.compose_ui.ui.theme.Brown
 import com.dev.james.booktracker.on_boarding.ui.components.StatefulRoundOutlineButton
-import com.dev.james.booktracker.on_boarding.ui.components.StatelessRoundOutlineButton
 import com.dev.james.booktracker.on_boarding.ui.navigation.OnBoardingNavigator
 import com.dev.james.booktracker.on_boarding.ui.viewmodel.OnBoardingWelcomeScreenViewModel
 import com.dev.james.on_boarding.R
@@ -43,9 +42,9 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Destination
 //@Preview(name = "On-Boarding welcome screen", widthDp = 320, heightDp = 700)
 fun OnBoardingWelcomeScreen(
-    modifier: Modifier = Modifier ,
-    onBoardingWelcomeScreenViewModel: OnBoardingWelcomeScreenViewModel = hiltViewModel() ,
-    onBoardingNavigator : OnBoardingNavigator
+    modifier: Modifier = Modifier,
+    onBoardingWelcomeScreenViewModel: OnBoardingWelcomeScreenViewModel = hiltViewModel(),
+    onBoardingNavigator: OnBoardingNavigator
 ) {
 
     val context = LocalContext.current
@@ -53,9 +52,9 @@ fun OnBoardingWelcomeScreen(
         mutableStateOf(1)
     }
     Box(
-        modifier = Modifier.fillMaxSize() ,
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
-    ){
+    ) {
 
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
@@ -70,21 +69,23 @@ fun OnBoardingWelcomeScreen(
             Spacer(modifier = Modifier.height(120.dp))
 
             Row(
-                verticalAlignment = Alignment.CenterVertically ,
-                horizontalArrangement = Arrangement.End ,
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween,
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
             ) {
+                CircularProgressIndicator(progress = position , count = 3)
+
                 StatefulRoundOutlineButton(
-                    text = if(position == 3) "Finish" else "Next"
-                ){
+                    text = if (position == 3) "Finish" else "Next"
+                ) {
                     //Toast.makeText(context, "Next button clicked", Toast.LENGTH_SHORT).show()
                     /*onBoardingWelcomeScreenViewModel.finishOnBoardingStatus()
                     onBoardingNavigator.openHome()*/
-                    if(position < 3){
+                    if (position < 3) {
                         position += 1
-                    }else {
+                    } else {
                         onBoardingWelcomeScreenViewModel.finishOnBoardingStatus()
                         onBoardingNavigator.openHome()
                     }
@@ -104,12 +105,11 @@ fun OnBoardingWelcomeScreen(
                 .background(color = Color.White)
                 .fillMaxWidth()
                 .align(Alignment.TopCenter)
-                .fillMaxHeight(0.55f)
-            ,
+                .fillMaxHeight(0.55f),
             contentAlignment = Alignment.Center
         ) {
             //pass in the lottie animation composable
-           LottieAnimationSection( position = position)
+            LottieAnimationSection(position = position)
         }
     }
 
@@ -120,22 +120,24 @@ fun OnBoardingWelcomeScreen(
 fun LottieAnimationSection(
     modifier: Modifier = Modifier,
     position: Int
-){
-    val anim = when(position){
+) {
+    val anim = when (position) {
         1 -> LottieCompositionSpec.RawRes(R.raw.books_reader_lottie)
         2 -> LottieCompositionSpec.RawRes(R.raw.notification_lottie)
         3 -> LottieCompositionSpec.RawRes(R.raw.monitor_lottie)
-        else -> { LottieCompositionSpec.RawRes(R.raw.books_reader_lottie)  }
+        else -> {
+            LottieCompositionSpec.RawRes(R.raw.books_reader_lottie)
+        }
     }
 
 
     Box(
-        contentAlignment = Alignment.Center ,
+        contentAlignment = Alignment.Center,
         modifier = Modifier.padding(16.dp)
-    ){
+    ) {
         AnimatedContent(
-            modifier = Modifier.fillMaxWidth() ,
-            targetState = anim ,
+            modifier = Modifier.fillMaxWidth(),
+            targetState = anim,
             transitionSpec = {
                 slideInHorizontally(
                     initialOffsetX = { it }
@@ -144,7 +146,7 @@ fun LottieAnimationSection(
                 ) + fadeOut()
             }
         ) {
-            val isLottiePlaying by remember{
+            val isLottiePlaying by remember {
                 mutableStateOf(true)
             }
             val animationSpeed by remember {
@@ -155,29 +157,29 @@ fun LottieAnimationSection(
             )
 
             val lottieAnimationState by animateLottieCompositionAsState(
-                composition = composition ,
-                iterations = LottieConstants.IterateForever ,
-                isPlaying = isLottiePlaying ,
-                speed = animationSpeed ,
+                composition = composition,
+                iterations = LottieConstants.IterateForever,
+                isPlaying = isLottiePlaying,
+                speed = animationSpeed,
                 restartOnPlay = true
             )
 
             LottieAnimation(
-                composition = composition ,
+                composition = composition,
                 lottieAnimationState,
                 modifier = Modifier.size(300.dp)
             )
         }
     }
-    
+
 }
 
 @OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun OnBoardingMessageBlock(
-    modifier: Modifier = Modifier ,
-    position : Int
-){
+    modifier: Modifier = Modifier,
+    position: Int
+) {
     /*val title = when(position){
         1 -> "Some title 1"
         2 -> "Some title 2"
@@ -190,21 +192,21 @@ fun OnBoardingMessageBlock(
         3 -> R.string.on_boarding_message_2
         else -> { 0 }
     }*/
-    val (title , body) = when(position){
-        1 -> Pair(R.string.on_boarding_title_1 , R.string.on_boarding_message_1)
-        2 -> Pair(R.string.on_boarding_title_2 , R.string.on_boarding_message_2)
-        3 -> Pair(R.string.on_boarding_title_3 , R.string.on_boarding_message_3)
-        else -> Pair(R.string.on_boarding_title_1 , R.string.on_boarding_message_1)
+    val (title, body) = when (position) {
+        1 -> Pair(R.string.on_boarding_title_1, R.string.on_boarding_message_1)
+        2 -> Pair(R.string.on_boarding_title_2, R.string.on_boarding_message_2)
+        3 -> Pair(R.string.on_boarding_title_3, R.string.on_boarding_message_3)
+        else -> Pair(R.string.on_boarding_title_1, R.string.on_boarding_message_1)
     }
 
     Column(
-        verticalArrangement = Arrangement.Center ,
+        verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = Modifier.padding(16.dp)
     ) {
         AnimatedContent(
-            modifier = Modifier.fillMaxWidth() ,
-            targetState = title ,
+            modifier = Modifier.fillMaxWidth(),
+            targetState = title,
             transitionSpec = {
                 slideInHorizontally(
                     initialOffsetX = { it }
@@ -214,9 +216,9 @@ fun OnBoardingMessageBlock(
             }
         ) { targetState ->
             Text(
-                text = stringResource(id = targetState) ,
-                style = BookAppTypography.h5 ,
-                textAlign = TextAlign.Center ,
+                text = stringResource(id = targetState),
+                style = BookAppTypography.h5,
+                textAlign = TextAlign.Center,
                 color = Color.White
             )
         }
@@ -225,8 +227,8 @@ fun OnBoardingMessageBlock(
         Spacer(modifier = Modifier.height(16.dp))
 
         AnimatedContent(
-            modifier = Modifier.fillMaxWidth() ,
-            targetState = body ,
+            modifier = Modifier.fillMaxWidth(),
+            targetState = body,
             transitionSpec = {
                 slideInHorizontally(
                     initialOffsetX = { it }
@@ -234,11 +236,11 @@ fun OnBoardingMessageBlock(
                     targetOffsetX = { -it }
                 ) + fadeOut()
             }
-        ){ targetState ->
+        ) { targetState ->
             Text(
-                text = stringResource(id = targetState) ,
-                style = BookAppTypography.body1 ,
-                textAlign = TextAlign.Center ,
+                text = stringResource(id = targetState),
+                style = BookAppTypography.body1,
+                textAlign = TextAlign.Center,
                 color = Color.White
             )
         }
@@ -246,6 +248,76 @@ fun OnBoardingMessageBlock(
     }
 }
 
+
+@Composable
+fun CircularProgressIndicator(
+    modifier: Modifier = Modifier,
+    progress: Int ,
+    count: Int
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceAround,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        for (item in 1..count) {
+
+            val indicatorSize by animateDpAsState(
+                targetValue = if (item == progress) 3.dp else 0.dp,
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessVeryLow
+                )
+            )
+            val indicatorColor by animateColorAsState(
+                targetValue = if (item == progress) Color.White.copy(alpha = 0.5f) else Color.Transparent
+            )
+            val size by animateDpAsState(
+                targetValue = if (item == progress) 30.dp else 20.dp,
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioMediumBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+            SingleIndicatorItem(
+                indicatorSize = indicatorSize,
+                indicatorColor = indicatorColor,
+                size = size
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+        /*  list.forEach { item ->
+
+          }*/
+    }
+
+}
+
+@Composable
+fun SingleIndicatorItem(
+    modifier: Modifier = Modifier,
+    indicatorSize: Dp,
+    size: Dp,
+    indicatorColor: Color
+) {
+    Box(
+        modifier = Modifier
+            .clip(shape = CircleShape)
+            .border(width = indicatorSize, color = indicatorColor, shape = CircleShape)
+            .size(size),
+        contentAlignment = Alignment.Center
+    ) {
+        Box(
+            modifier = Modifier
+                .clip(shape = CircleShape)
+                .background(color = Color.White)
+                .size(size / 2)
+        ) {
+
+        }
+
+    }
+}
 
 
 
