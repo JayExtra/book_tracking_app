2eaa16e81a711fc06c23efcd0ad4071fd7e69515
James Gitonga <40741293+JayExtra@users.noreply.github.com>
2024-01-03 00:20:30 +0300

    BUG FIX: user password exception on pdf reading using iText library fixed

--- domain/src/main/java/com/dev/james/domain/usecases/FetchPdfBooks.kt
+++ domain/src/main/java/com/dev/james/domain/usecases/FetchPdfBooks.kt
@@ -1,13 +1,19 @@
 package com.dev.james.domain.usecases
 
 import android.content.Context
+import android.net.Uri
 import android.os.Build
 import android.provider.MediaStore
 import androidx.annotation.RequiresApi
 import androidx.core.net.toUri
 import com.dev.james.booktracker.core.common_models.Book
+import com.itextpdf.text.exceptions.BadPasswordException
+import com.itextpdf.text.pdf.PdfReader
+import com.itextpdf.text.pdf.ReaderProperties
 import com.tom_roush.pdfbox.android.PDFBoxResourceLoader
 import com.tom_roush.pdfbox.pdmodel.PDDocument
+import com.tom_roush.pdfbox.pdmodel.encryption.InvalidPasswordException
+import com.tom_roush.pdfbox.pdmodel.encryption.StandardDecryptionMaterial
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -15,6 +21,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import timber.log.Timber
 import java.io.File
+import java.io.IOException
 import javax.inject.Inject
 
 class FetchPdfBooks
@@ -24,6 +31,7 @@ class FetchPdfBooks
 
     companion object {
         const val TAG = "FetchPdfBooks"
+        const val USER_PASSWORD = " "
     }
 
     init {
@@ -32,50 +40,69 @@ class FetchPdfBooks
 
     @RequiresApi(Build.VERSION_CODES.Q)
     suspend operator fun invoke() {
-        try{
+        try {
             getPdfInStorage { files ->
                 Timber.tag(TAG).d("pdf list : $files")
+               // Timber.tag(TAG).d("file at pos 19 ${files[19]}")
                 val processedFiles = files.map { file ->
-                    val document = PDDocument.load(file)
-
-                    if(document.isEncrypted){
-                        Book()
-                    }else{
-                        val pageSize = document.numberOfPages
-                        val pdfTitle = document.documentInformation.title
-                        val pdfAuthor = document.documentInformation.author
-                        val pdfId = document.documentId.toString()
-                        Book(
-                            bookId = pdfId ,
-                            bookAuthors = listOf(pdfAuthor) ,
-                            bookUri = file.toUri() ,
-                            bookTitle = pdfTitle ,
-                            bookPagesCount = pageSize
-                        )
-                    }
-
+                    extractPdfMetadata(file.toUri())
+                }.filter { book ->
+                    book.bookPagesCount!! > 20
                 }
 
                 Timber.tag(TAG).d("pdf list : $processedFiles")
 
             }
-        }catch (e : Exception){
+        } catch (e: InvalidPasswordException) {
             Timber.tag(TAG).d("error fetching pdf : ${e.localizedMessage}")
         }
 
     }
 
-    private suspend fun getPdfInStorage( setPdfFiles : (files : List<File>) -> Unit){
-        withContext(Dispatchers.IO){
+    private fun extractPdfMetadata(pdfUri : Uri) : Book {
+        return try {
+            val inputStream = context.contentResolver.openInputStream(pdfUri)
+            val pdfReader = PdfReader(inputStream)
+            val pdfInfo = pdfReader.info
+            val pagesCount = pdfReader.numberOfPages
+            val author = pdfInfo["Author"] ?: pdfInfo["Creator"] ?: "No author information"
+            val publisher = pdfInfo["Producer"] ?: "No publisher information"
+            val title = pdfInfo["Title"] ?: "No title"
+
+            pdfReader.close()
+            Book(
+                bookPagesCount = pagesCount ,
+                bookAuthors = listOf(author) ,
+                bookUri = pdfUri ,
+                publisher = publisher ,
+                bookTitle = title
+            )
+        }catch (e : BadPasswordException ){
+            Timber.tag(TAG).d("Bad password exception! => ${e.localizedMessage}")
+            Book(
+                bookPagesCount = 0
+            )
+        }catch (e : IOException){
+            Timber.tag(TAG).d("IOException! => ${e.localizedMessage}")
+            Book(
+                bookPagesCount = 0
+            )
+        }
+
+    }
+
+    private suspend fun getPdfInStorage(setPdfFiles: suspend (files: List<File>) -> Unit) {
+        withContext(Dispatchers.IO) {
             try {
                 val uri = MediaStore.Files.getContentUri("external")
                 val projection = arrayOf(MediaStore.Files.FileColumns.DATA)
                 val selection = MediaStore.Files.FileColumns.MIME_TYPE + "=?"
                 val selectionArgs = arrayOf("application/pdf")
-                val cursor = context.contentResolver.query(uri, projection, selection, selectionArgs, null)
+                val cursor =
+                    context.contentResolver.query(uri, projection, selection, selectionArgs, null)
                 val pdfList = mutableListOf<File>()
 
-                if(cursor != null){
+                if (cursor != null) {
                     val pdfPathIndex = cursor.getColumnIndex(MediaStore.Files.FileColumns.DATA)
                     while (cursor.moveToNext()) {
                         if (pdfPathIndex != -1) {
@@ -88,12 +115,12 @@ class FetchPdfBooks
                     }
                     cursor.close()
                     setPdfFiles(pdfList)
-                }else{
+                } else {
                     Timber.tag(TAG).d("Cursor returned null!")
                     setPdfFiles(emptyList<File>())
                 }
 
-            }catch (e : Exception){
+            } catch (e: Exception) {
                 Timber.tag(TAG).e("Error => Exception: ${e.localizedMessage}")
                 e.printStackTrace()
             }
@@ -101,31 +128,4 @@ class FetchPdfBooks
 
     }
 
-   /* @RequiresApi(Build.VERSION_CODES.Q)
-    private fun getPDFUriList(): List<String> {
-        val pdfList = mutableListOf<String>()
-        val externalStorageUri = MediaStore.Downloads.EXTERNAL_CONTENT_URI
-        val selection = MediaStore.Downloads.MIME_TYPE + " = ?"
-        val selectionArgs = arrayOf("application/pdf")
-        val sortOrder = MediaStore.Downloads.DATE_ADDED + " DESC"
-
-        val cursor = context.contentResolver.query(
-            externalStorageUri,
-            null,
-            selection,
-            selectionArgs,
-            sortOrder
-        )
-
-        if (cursor != null) {
-            while (cursor.moveToNext()) {
-                val pdfFilePath =
-                    cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Downloads.DATA))
-                pdfList.add(pdfFilePath)
-            }
-
-            cursor.close()
-        }
-        return pdfList
-    }*/
 }
